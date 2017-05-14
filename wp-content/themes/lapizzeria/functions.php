531defdc684c6e6d1096d50d703850e8eac2ac62
Treisy Jimenez <treisyj@thehangar.cr>
2017-05-14 16:48:16 -0600

    Creación el menú de redes sociales

--- wp-content/themes/lapizzeria/functions.php
+++ wp-content/themes/lapizzeria/functions.php
@@ -1,5 +1,5 @@
 <?php
-
+// Add styles
 function lapizzeria_styles() {
     // Registrar los estillos
     wp_register_style('normalize', get_template_directory_uri(), '/css/normalize.css', array(), '7.0.0');
@@ -12,4 +12,13 @@ function lapizzeria_styles() {
 
 add_action('wp_enqueue_scripts', 'lapizzeria_styles');
 
+// Create menus
+function lapizzeria_menus() {
+    register_nav_menus(array(
+        'header-menu' => _('Header Menu', lapizzeria),
+        'social-menu' => _('Social Menu', lapizzeria)
+    ));
+}
+
+add_action('init', lapizzeria_menus);
 ?>
--- wp-content/themes/lapizzeria/header.php
+++ wp-content/themes/lapizzeria/header.php
@@ -9,3 +9,12 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <header class="encabezado-sitio">
+        <div class="contenedor">
+            <div class="logo">
+                <a href="<?php echo esc_url( home_url('/') ); ?>">
+                    <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="">
+                </a>
+            </div>
+        </div>
+    </header>
