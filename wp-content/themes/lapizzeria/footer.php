cf7edc3ca4382eb4ff62c6753c50bcd48536a8ec
Treisy Jimenez <treisyj@thehangar.cr>
2017-05-19 10:26:29 -0600

    Agregar el footer del sitio.

--- wp-content/themes/lapizzeria/footer.php
+++ wp-content/themes/lapizzeria/footer.php
@@ -1,3 +1,22 @@
+    <footer>
+        <?php 
+            $args = array(
+                'theme_location' => 'header-menu',
+                'container' => 'nav',
+                'after' => '<span class="separador"> | </span>'    
+            );
+
+            wp_nav_menu($args);
+        ?>
+
+        <div class="ubicacion">
+            <p>8179 Bay Avenue Mountain View, CA 94043</p>
+            <p>Teléfono: +1-92-456-7890</p>
+        </div>
+
+        <p class="copyright">Todos los derechos reservados <?php echo date('Y'); ?></p>
+    </footer>
+    
     <?php wp_footer() ?>
 </body>
 </html>
